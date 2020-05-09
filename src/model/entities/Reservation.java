9ad7f5f79ce9005de501bb39351fdb6ee79b7250
Bruno Bezerra Lima <brunobezerralima@gmail.com>
2020-05-08 22:40:16 -0300

    Very bad solution

---
+++ src/model/entities/Reservation.java
@@ -0,0 +1,63 @@
+package model.entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Reservation {
+
+	private Integer roomNumber;
+	private Date checkin;
+	private Date checkout;
+
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+	public Reservation(Integer roomNumber, Date checkin, Date checkout) {
+		this.roomNumber = roomNumber;
+		this.checkin = checkin;
+		this.checkout = checkout;
+	}
+
+	public Integer getRoomNumber() {
+		return roomNumber;
+	}
+
+	public void setRoomNumber(Integer roomNumber) {
+		this.roomNumber = roomNumber;
+	}
+
+	public Date getCheckin() {
+		return checkin;
+	}
+
+	public Date getCheckout() {
+		return checkout;
+	}
+
+	public long duration() {
+		// Calcula a diferença em milissegundos entre duas datas;
+		long diff = checkout.getTime() - checkin.getTime();
+		// Converte milissegundos para dias;
+		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+	}
+
+	public void updateDates(Date checkIn, Date checkOut) {
+		this.checkin = checkIn;
+		this.checkout = checkOut;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("Room ");
+		sb.append(roomNumber);
+		sb.append(", check-in: ");
+		sb.append(sdf.format(checkin));
+		sb.append(", check-out: ");
+		sb.append(sdf.format(checkout));
+		sb.append(", ");
+		sb.append(duration());
+		sb.append(" nights");
+		return sb.toString();
+	}
+}
